5a80d6f66ea04278fa43808dcb0113d718902e1f
Zsolt Varga <90441189+VZsolt23@users.noreply.github.com>
2023-01-16 17:33:21 +0100

    Some changes and adding turtlesim_saver.py

--- publisher.py
+++ publisher.py
@@ -1,16 +1,15 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # license removed for brevity
 import rospy
-from std_msgs.msg import String
+from std_msgs.msg import Int16
 
-def talker():
-    pub = rospy.Publisher('chatter', String, queue_size=10)
-    rospy.init_node('talker', anonymous=True)
+def number_publisher():
+    pub = rospy.Publisher('number_out', Int16, queue_size=10)
+    rospy.init_node('int_publisher')
     rate = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
-        hello_str = "hello world %s" % rospy.get_time()
-        rospy.loginfo(hello_str)
-        pub.publish(hello_str)
+        num_msg = 23
+        pub.publish(num_msg)
         rate.sleep()
 
 if __name__ == '__main__':
--- subscriber.py
+++ subscriber.py
@@ -1,9 +1,9 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import rospy
-from std_msgs.msg import String
+from std_msgs.msg import Int16
 
-def callback(data):
-    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+def num_cb(data):
+    print("Direct: ", data, " / ", "Sub Obj: ", data.data)
     
 def listener():
 
@@ -12,9 +12,9 @@ def listener():
     # anonymous=True flag means that rospy will choose a unique
     # name for our 'listener' node so that multiple listeners can
     # run simultaneously.
-    rospy.init_node('listener', anonymous=True)
+    rospy.init_node('int_subscriber', anonymous=True)
 
-    rospy.Subscriber("chatter", String, callback)
+    rospy.Subscriber("number_out", Int16, num_cb)
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
